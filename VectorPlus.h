89e58dd606897de415ef1d0087760e9d1ed21401
Jesusemora <32273722+Jesusemora@users.noreply.github.com>
2023-03-13 17:45:39 -0300

    Add files via upload
    fixed a bug
    started cubemap textures
    added features

--- VectorPlus.h
+++ VectorPlus.h
@@ -350,6 +350,7 @@ public:
 		x = obj.x;
 		y = obj.y;
 		z = obj.z;
+		//return *this;
 	}
 	
 	Vector3 operator=(const Vector2 & obj)
@@ -1284,6 +1285,7 @@ private:
 	 */
 	
 	bool mipMaps = true;
+	int flags = 0;
 	
 	void setParams()
 	{
@@ -1372,7 +1374,7 @@ public:
 	void createTex(char *filePath, bool alpha)
 	{
 		imageO = SOIL_load_image(filePath, &width, &height, 0, SOIL_LOAD_RGBA);
-		isAlpha = true;
+		isAlpha = alpha;
 	}
 	
 	void BindTexture(int id, GLuint *textureID)
@@ -1389,7 +1391,14 @@ public:
 			this->setParams();
 			//std::cout << glGetError() << std::endl;
 			
-			glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, this->width, this-> height, 0, GL_RGB, GL_UNSIGNED_BYTE, this->imageO);
+			if(isAlpha)
+			{
+				glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, this->width, this-> height, 0, GL_RGBA, GL_UNSIGNED_BYTE, this->imageO);
+			}
+			else
+			{
+				glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, this->width, this-> height, 0, GL_RGB, GL_UNSIGNED_BYTE, this->imageO);
+			}
 			
 			//glGenerateMipmap(GL_TEXTURE_2D);
 			this->setMipmap();
@@ -1403,7 +1412,14 @@ public:
 		{
 			glActiveTexture(GL_TEXTURE0 + id);
 			//glBindTexture(GL_TEXTURE_2D, textureID[id]);
-			glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, this->width, this->height, 0, GL_RGB, GL_UNSIGNED_BYTE, this->imageO);
+			if(isAlpha)
+			{
+				glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, this->width, this-> height, 0, GL_RGBA, GL_UNSIGNED_BYTE, this->imageO);
+			}
+			else
+			{
+				glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, this->width, this->height, 0, GL_RGB, GL_UNSIGNED_BYTE, this->imageO);
+			}
 			//this->setMipmap();
 			//glGenerateMipmap(GL_TEXTURE_2D);//TODO image is in CPU memory AND GPU memory (in theory)
 			//we need a way to send ALL of the texture data into GPU memory and keep it there until program ends
@@ -1414,9 +1430,78 @@ public:
 	{
 		//glActiveTexture(GL_TEXTURE0);
 		//delete textures
-		if(imageO)
+		//if((flags & 2) == 0)
+		//{
+		free(imageO);
+		//}
+	}
+};
+
+class Skybox {
+public:
+	std::vector<int> width;
+	std::vector<int> height;
+	unsigned char* imageO1;
+	unsigned char* imageO2;
+	unsigned char* imageO3;
+	unsigned char* imageO4;
+	unsigned char* imageO5;
+	unsigned char* imageO6;
+	GLuint textureID;
+	
+	void createTex(char *filePath[6])
+	{
+		width.push_back(0);
+		height.push_back(0);
+		imageO1 = SOIL_load_image(filePath[0], &this->width[0], &this->height[0], 0, SOIL_LOAD_RGB);
+		width.push_back(0);
+		height.push_back(0);
+		imageO2 = SOIL_load_image(filePath[1], &this->width[1], &this->height[1], 0, SOIL_LOAD_RGB);
+		width.push_back(0);
+		height.push_back(0);
+		imageO3 = SOIL_load_image(filePath[2], &this->width[2], &this->height[2], 0, SOIL_LOAD_RGB);
+		width.push_back(0);
+		height.push_back(0);
+		imageO4 = SOIL_load_image(filePath[3], &this->width[3], &this->height[3], 0, SOIL_LOAD_RGB);
+		width.push_back(0);
+		height.push_back(0);
+		imageO5 = SOIL_load_image(filePath[4], &this->width[4], &this->height[4], 0, SOIL_LOAD_RGB);
+		width.push_back(0);
+		height.push_back(0);
+		imageO6 = SOIL_load_image(filePath[5], &this->width[5], &this->height[5], 0, SOIL_LOAD_RGB);
+	}
+	
+	void BindTexture()
+	{
+		if(imageO1 && imageO2 && imageO3 && imageO4 && imageO5 && imageO6)
 		{
-			free(imageO);
+			glGenTextures(1, &textureID);
+			glActiveTexture(GL_TEXTURE2);//we'll see
+			glBindTexture(GL_TEXTURE_CUBE_MAP, textureID);
+			
+			glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X, 0, GL_RGB, width[0], height[0], 0, GL_RGB, GL_UNSIGNED_BYTE, this->imageO1);
+			free(this->imageO1);
+			
+			glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + 1, 0, GL_RGB, width[1], height[1], 0, GL_RGB, GL_UNSIGNED_BYTE, this->imageO2);
+			free(this->imageO2);
+			
+			glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + 2, 0, GL_RGB, width[2], height[2], 0, GL_RGB, GL_UNSIGNED_BYTE, this->imageO3);
+			free(this->imageO3);
+			
+			glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + 3, 0, GL_RGB, width[3], height[3], 0, GL_RGB, GL_UNSIGNED_BYTE, this->imageO4);
+			free(this->imageO4);
+			
+			glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + 4, 0, GL_RGB, width[4], height[4], 0, GL_RGB, GL_UNSIGNED_BYTE, this->imageO5);
+			free(this->imageO5);
+			
+			glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + 5, 0, GL_RGB, width[5], height[5], 0, GL_RGB, GL_UNSIGNED_BYTE, this->imageO6);
+			free(this->imageO6);
+				
+			glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+			glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+			glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
+			glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+			glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 		}
 	}
 };
@@ -1514,9 +1599,9 @@ public:
 		
 		for(int abc = 0; abc < this->numindices; abc+= 3)
 		{
-			Vector3 v0 = Vector3(this->vertices[this->indices[abc] * 3], this->vertices[this->indices[abc] * 3 + 1], this->vertices[this->indices[abc] * 3 + 2]);
-			Vector3 v1 = Vector3(this->vertices[this->indices[abc + 1] * 3], this->vertices[this->indices[abc + 1] * 3 + 1], this->vertices[this->indices[abc + 1] * 3 + 2]);
-			Vector3 v2 = Vector3(this->vertices[this->indices[abc + 2] * 3], this->vertices[this->indices[abc + 2] * 3 + 1], this->vertices[this->indices[abc + 2] * 3 + 2]);
+			Vector3 v0(this->vertices[this->indices[abc] * 3], this->vertices[this->indices[abc] * 3 + 1], this->vertices[this->indices[abc] * 3 + 2]);
+			Vector3 v1(this->vertices[this->indices[abc + 1] * 3], this->vertices[this->indices[abc + 1] * 3 + 1], this->vertices[this->indices[abc + 1] * 3 + 2]);
+			Vector3 v2(this->vertices[this->indices[abc + 2] * 3], this->vertices[this->indices[abc + 2] * 3 + 1], this->vertices[this->indices[abc + 2] * 3 + 2]);
 			
 			Vector2 uv0 = Vector2(this->uvs[this->indices[abc] * 2], this->uvs[this->indices[abc] * 2 + 1]);
 			Vector2 uv1 = Vector2(this->uvs[this->indices[abc + 1] * 2], this->uvs[this->indices[abc + 1] * 2 + 1]);
